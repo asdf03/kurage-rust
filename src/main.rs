b113747a35a85888bcf01a7d4cc8f6179db458fa
Masaya Nakatsuka <120952058+asdf03@users.noreply.github.com>
2024-06-01 08:12:01 +0000

    ユーザー間でのデータ同期に成功

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,27 @@
-use std::fmt::format;
+use actix_web::{web, App, HttpServer};
+use std::sync::Mutex;
 
-use actix_web::{get, web, App, HttpServer};
-
-struct AppState {
-    app_name: String,
+struct AppStateWithCounter {
+    counter: Mutex<i32>,
 }
 
-#[get("/")]
-async fn index(data: web::Data<AppState>) -> String {
-    let app_name = &data.app_name;
-    format!("Hello {app_name}!")
+async fn index(data: web::Data<AppStateWithCounter>) -> String {
+    let mut counter = data.counter.lock().unwrap();
+    *counter += 1;
+
+    format!("Request number: {counter}")
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    let counter = web::Data::new(AppStateWithCounter {
+        counter: Mutex::new(0),
+    });
+
+    HttpServer::new(move || {
         App::new()
-            .app_data(web::Data::new(AppState {
-                app_name: String::from("Actix Web"),
-            }))
-            .service(index)
+            .app_data(counter.clone()) // ユーザー間でデータが共有される
+            .route("/", web::get().to(index))
     })
     .bind(("127.0.0.1", 8080))?
     .run()
