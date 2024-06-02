ad65a65229edecb3974302de878da7de0f90c370
Masaya Nakatsuka <120952058+asdf03@users.noreply.github.com>
2024-06-02 05:09:43 +0000

    リソース設置を外部関数へ移動

---
+++ src/05-main.rs
@@ -0,0 +1,24 @@
+use actix_web::{guard, web, App, HttpResponse, HttpServer};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(
+                web::scope("/")
+                    // curl -H "Host: www.rust-lang.org" http://127.0.0.1:8080
+                    .guard(guard::Header("Host", "www.rust-lang.org"))
+                    .route("", web::to(|| async { HttpResponse::Ok().body("www") })),
+            )
+            .service(
+                web::scope("/")
+                    // curl -H "Host: users.rust-lang.org" http://127.0.0.1:8080
+                    .guard(guard::Header("Host", "users.rust-lang.org"))
+                    .route("", web::to(|| async { HttpResponse::Ok().body("user") }))
+            )
+            .route("/", web::to(HttpResponse::Ok))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
