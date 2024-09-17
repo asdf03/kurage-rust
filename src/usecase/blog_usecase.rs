f2603bb915b52b7dd4b350755bcc1c008b084690
Masaya Nakatsuka <nmsuteado2@yahoo.co.jp>
2024-09-17 20:02:40 +0900

    blog_usecase:ルートページにmarkdownファイル配下のページすべてを表示するようにできた

--- src/usecase/blog_usecase.rs
+++ src/usecase/blog_usecase.rs
@@ -1,6 +1,7 @@
 use std::{
-  fs,
-  path::Path
+  fs::{self, File},
+  path::Path,
+  io::{BufRead, BufReader}
 };
 
 use axum::{
@@ -13,15 +14,44 @@ use axum::{
 
 use comrak::{markdown_to_html, ComrakOptions};
 
+use tera::{Tera, Context};
+use serde::Serialize;
+
+#[derive(Debug, Serialize)]
+struct BlogPost {
+  url_path: String,
+  title: String,
+}
+
 
 async fn load_template(template_name: &str) -> String {
   let template_path = Path::new("static").join("html").join(template_name);
   fs::read_to_string(template_path).expect(&format!("Failed to read {}", template_name))
 }
 
+
+fn process_markdown_file(path: &Path, blog_posts: &mut Vec<BlogPost>) -> std::io::Result<()> {
+  let file_stem = path.file_stem().ok_or_else(|| std::io::Error::new(std::io::ErrorKind::Other, "Invalid file name"))?;
+let url_path = format!("/blog/{}", file_stem.to_string_lossy()); 
+  let file = File::open(path)?;
+  let reader = BufReader::new(file);
+  
+  for line in reader.lines() {
+    let line = line?;
+    if line.starts_with("## ") {
+      let title = line.trim_start_matches("## ").to_string();
+      blog_posts.push(BlogPost { url_path, title });
+      break;
+    }
+  }
+  
+  Ok(())
+}
+
 pub async fn root_page() -> impl IntoResponse {
   let current_dir = std::env::current_dir().expect("Failed to get current_dir path");
   let folder_path = current_dir.join("markdown_files");
+  let mut blog_posts = Vec::new();
   
   match fs::read_dir(folder_path) {
     Ok(entries) => {
@@ -30,7 +60,9 @@ pub async fn root_page() -> impl IntoResponse {
           Ok(entry) => {
             let path = entry.path();
             if path.is_file() {
-              println!("OK");
+              if let Err(e) = process_markdown_file(&path, &mut blog_posts) {
+                println!("Error processing file {:?}: {}", path, e);
+              }
             }
           }
           Err(e) => println!("Error reading entry: {}", e),
@@ -38,17 +70,18 @@ pub async fn root_page() -> impl IntoResponse {
       }
     }
     Err(e) => println!("Error reading directory: {}", e),
-
-    // let blog_path = blog_page.path();
-    // if blog_path.is_file() {
-    //   println!("Processing file: {:?}", blog_path);
-    //   // ここでファイルに対する処理を実行
-    // }
-
   }
 
+  println!("{:#?}", blog_posts);
+
+  let mut tera = Tera::default();
+  tera.add_raw_template("root", &load_template("root.html").await).unwrap();
+
+  let mut context = Context::new();
+  context.insert("blog_posts", &blog_posts);
+
   let html_header = load_template("header.html").await;
-  let html_root = load_template("root.html").await;
+  let html_root = tera.render("root", &context).unwrap();
   let html_footer = load_template("footer.html").await;
 
   let html_page = format!("{}{}{}", html_header, html_root, html_footer);
