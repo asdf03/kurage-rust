1c8ec13500265cc04d9018bd9c2693f818aa5aa9
Masaya Nakatsuka <nmsuteado2@yahoo.co.jp>
2024-10-05 21:12:08 +0900

    main関数の不要なimportを削除

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,4 @@
-use axum::{
-	extract::Path as AxumPath, response::Html, routing::{get, post}, Extension, Router
-};
-use comrak::{markdown_to_html, ComrakOptions};
-use std::{fs, path::Path, sync::{Arc, Mutex}};
+use axum;
 
 use kurage_rust::create_router;
 
