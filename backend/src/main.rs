e0afeeca8de4e14494a5e0eb0b4c8715026e49af
Masaya Nakatsuka <nmsuteado2@yahoo.co.jp>
2024-10-13 15:24:05 +0900

    backend,frontendに処理を分け、テスト用のエンドポイントで疎通確認完了

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,13 +1,20 @@
-use axum;
+use axum::http::{HeaderValue, Method};
+// use http::{Method, HeaderValue};
 
 use kurage_rust::create_router;
+use tower_http::cors::{Any, CorsLayer, AllowMethods, AllowOrigin};
+use tower::ServiceBuilder;
 
 #[tokio::main]
 async fn main() {
 
-	let app = create_router();
+	let cors = CorsLayer::new()
+		.allow_methods(AllowMethods::list([Method::GET]))
+		.allow_origin(AllowOrigin::list(vec!["http://localhost:3000".parse().unwrap()]));
+
+	let app = create_router().layer(cors);
 	
-	let listener = tokio::net::TcpListener::bind("0.0.0.0:8080")
+	let listener = tokio::net::TcpListener::bind("127.0.0.1:8000")
 		.await
 		.unwrap();
 
